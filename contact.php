d44fbec045231666061c8600ca6de6a4a92ffb14
Cooper Featherstone <cooperfeatherstone13@gmail.com>
2019-11-11 18:29:35 -0600

    update

--- contact.php
+++ contact.php
@@ -1,15 +1,17 @@
 <?php
 
-if(isset($_POST['sbmit'])){
+if(isset($_POST['submit'])){
 
 	$name = $_POST['name'];
 	$subject = $_POST['subject'];
-	$mail = $_POST['email'];
+	$mailFrom = $_POST['email'];
 	$message = $_POST['message'];
 
-	
-
-	mail();
+	$mailTo = "cooperfeatherstone13@gmail.com";
+	$headers = "From: ".$mailFrom;
+	$txt = "You have received an email from ".$name.".\n\n".$message;
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: index.php?mailsend");
 }
 
 
